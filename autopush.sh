6aa8b5085e7dc360e3e8da6cc52dd4d8f5655853
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-06-04 11:44:39 +0200

    Add cronjob script for periodically squashing and pushing.

---
+++ autopush.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Squash unpushed commits into a single commit and then push to the remote
+# repository.
+
+
+git fetch origin
+# Check if there are any unpushed commits
+if [ "$(git rev-list --count HEAD ^origin/$(git rev-parse --abbrev-ref HEAD))" -eq 0 ]; then
+    echo "No unpushed commits found."
+    exit 0
+    fi
+
+# Squash the unpushed commits into a single commit
+git reset --soft origin/$(git rev-parse --abbrev-ref HEAD)
+git commit -m "Autosync: Squashed unpushed commits"
+# Push the squashed commit to the remote repository
+git push origin HEAD:$(git rev-parse --abbrev-ref HEAD) --force
+echo "Unpushed commits have been squashed and pushed to the remote repository."
+exit 0
