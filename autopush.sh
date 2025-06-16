6a7bd25797e1a1663de2fbce247f1b632a6eea2f
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-06-16 12:38:35 +0200

    Bruk notify-send for varsling når autosync kjører.

--- autopush.sh
+++ autopush.sh
@@ -3,12 +3,15 @@
 # Squash unpushed commits into a single commit and then push to the remote
 # repository.
 
+notify-send "Autosync" "⏳ Checking for unpushed commits..."
+
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
 git fetch origin
 # Check if there are any unpushed commits
 if [ "$(git rev-list --count HEAD ^origin/$(git rev-parse --abbrev-ref HEAD))" -eq 0 ]; then
     echo "$TIMESTAMP: No unpushed commits found." >> ~/autopush.log
+    notify-send "Autosync" "🍎 No unpushed commits found."
     exit 0
     fi
 
@@ -19,5 +22,6 @@ AI_MSG=$(node ai-commit/index.js --message-only 2>/dev/null)
 git commit -m "Autosync: $AI_MSG"
 # Push the squashed commit to the remote repository
 git push origin HEAD:$(git rev-parse --abbrev-ref HEAD) --force
-echo "$TIMESTAMP: Unpushed commits have been squashed and pushed to the remote repository.\n" >> ~/autopush.log
+echo "$TIMESTAMP: Unpushed commits have been squashed and pushed to the remote repository." >> ~/autopush.log
+notify-send "Autosync" "🍏 Unpushed commits have been squashed and pushed to the remote repository."
 exit 0
