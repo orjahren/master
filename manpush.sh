134600df994c61f9f8b85e91d4fdc828d33486d8
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-06-19 16:16:51 +0200

    Ikke bruk varslinger når scriptet uansett kun kjøres interaktivt. [no ci]

--- manpush.sh
+++ manpush.sh
@@ -10,22 +10,21 @@ git fetch origin
 # Get folder from ../akademika/master/essay 
 rsync --recursive ../akademika/master/essay/ essay 
 if [ $? -ne 0 ]; then
-    notify-send "Mansync" "Error during rsync. Please check the logs."
+    echo "Mansync: Error during rsync. Please check the logs."
     exit 1
 fi  
 
 git add essay
 
 # NOTE: Assumes my fork -> https://github.com/orjahren/ai-commit
-AI_MSG=$(node ../ai-commit-message/index.js --message-only 2>> ~/autopush.log)
+AI_MSG=$(node ../ai-commit-message/index.js --message-only 2>> ~/manpush.log)
 if [ $? -ne 0 ]; then
-    notify-send "Mansync" "Unable to generate commit message. Please check the logs."
+    echo "Mansync: Unable to generate commit message. Please check the logs."
     exit 1
 fi
 
 git commit -m "Mansync: $AI_MSG"
 # Push the squashed commit to the remote repository
 git push origin HEAD:$(git rev-parse --abbrev-ref HEAD)
-echo "$TIMESTAMP: Unpushed commits have been squashed and pushed to the remote repository." >> ~/autopush.log
-notify-send "Mansync" "🍏 Unpushed commits have been squashed and pushed to the remote repository."
+echo "$TIMESTAMP: Unpushed commits have been squashed and pushed to the remote repository." >> ~/manpush.log
 exit 0
